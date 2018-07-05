74fa35a3f4440f449e2cf09a1511d5dff7d8bb39
nh-cx <colin2002308@163.com>
2018-07-05 11:28:50 +0800

    up Other-Excel-Update.py

--- Monitor_80Port.py
+++ Monitor_80Port.py
@@ -8,18 +8,19 @@ import time
 
 monitorPort = '80'
 
-GetStr = os.popen('netstat -tulnp').read()
-
-print(GetStr)
-
-StrINFO = re.split('\n', GetStr)[2:-1]
+Found = False
 
 while True:
+    GetStr = os.popen('netstat -tulnp').read()
+    StrINFO = re.split('\n', GetStr)[2:-1]
+
+    Found = False
     for x in StrINFO:
         x = str(x).split()[3]
         x = re.match('.*:(\d+)$', x).groups()[0]
-        if x == monitorPort:
+        if str(x) == monitorPort:
+            Found = True
             print('(TCP/80)服务已经被打开')
-            continue
-    print('等待一秒重新开始监控！')
+    if not Found:
+        print('等待五秒重新开始监控！')
     time.sleep(5)
