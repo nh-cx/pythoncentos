348af5ba4382a1cbf545bdc84441a6aad1867238
nh-cx <colin2002308@163.com>
2018-07-06 21:47:06 +0800

    update module and demo

--- DemoMainPingSSHData.py
+++ DemoMainPingSSHData.py
@@ -6,4 +6,14 @@ import ModuleNetworkSsh
 import DemoNetworkDataIpDns
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
+    for i in DemoNetworkDataIpDns.IP_ADDRESS_Demo_List:
+        PingResult = ModuleNetworkPing.icmp_packet(i)
+        print('IP:'+i+'  '+PingResult)
+        if 'have reply' == PingResult:
+            result = ModuleNetworkSsh.ssh_client_sigin(i, user_name='py', pass_wd='~123456', ins_command='/ip add print')
+            if result is None:
+                print("无法SSH Host:"+i)
+            else:
+                print(result)
+        print()
