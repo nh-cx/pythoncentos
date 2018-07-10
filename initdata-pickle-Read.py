2cd2e05190669d0653e7e3bb3accd5d27ce6c167
nh-cx <colin2002308@163.com>
2018-07-10 19:47:42 +0800

    Update

--- initdata-pickle-Read.py
+++ initdata-pickle-Read.py
@@ -2,9 +2,23 @@
 # -*- coding=utf-8 -*-
 # 学习专用
 import pickle
-dbfile = open('pickle-db.pl', 'rb')
-db = pickle.load(dbfile)
-for i in db:
-    print(i, '=>', db[i])
+def open_pickle_file(pl_file):
+    dbfile = open(pl_file, 'rb')
+    db = pickle.load(dbfile)
+    return db
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
+    mydb = open_pickle_file('pickle-db.pl')
+    for i in mydb:
+        print(i,'==>',mydb[i])
+
+    print(mydb['tan2']['pay'])
+    mydb['tan2']['pay'] = int(mydb['tan2']['pay']*1.6)
+
+    dbfile = open('pickle-db.pl', 'wb')
+    pickle.dump(mydb, dbfile)
+    dbfile.close()
+
+    mydb = open_pickle_file('pickle-db.pl')
+    for i in mydb:
+        print(i, '==>', mydb[i])
