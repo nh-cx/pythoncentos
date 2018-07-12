3f3d17e762a4ea2aaf2023cdec406776b511cc72
nh-cx <colin2002308@163.com>
2018-07-12 22:10:38 +0800

    Update

---
+++ MathModule.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3.6
+# -*- coding=utf-8 -*-
+# 学习专用
+import time
+import os
+import threading
+
+def math_multi(x,y,z):
+    # 打印进程和线程ID
+    # print('pid tid==>',os.getpid(),threading.currentThread().ident)
+    i = 1
+    while i<10:
+        sum = x*y*z
+        x+=1
+        y+=1
+        z+=1
+        i+=1
+        time.sleep(1)
+
+    return sum, os.getpid(), threading.currentThread().ident
+
+# if __name__ == "__main__":
---
+++ ProcessAndTread.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3.6
+# -*- coding=utf-8 -*-
+# 学习专用
+from MathModule import math_multi
+from multiprocessing import Pool as ProcessPool
+from multiprocessing.pool import ThreadPool
+from multiprocessing import freeze_support
+import random
+
+# 多线程
+# freeze_support()
+# Windows平台要加上这句，避免RuntimeError
+
+# 有效控制并发进程或者线程数，不设置为内核数
+# 得到内核数的方法
+# cpus = multiprocessing.cpu_count()
+pool = ProcessPool(processes=4)
+
+results = []
+for i in range(0,10):
+    x = random.randint(1,10)
+    y = random.randint(1,10)
+    z = random.randint(1,10)
+    result = pool.apply_async(math_multi,args=(x,y,z))
+    results.append(result)
+
+# pool.apply_async采用异步方式调用task
+# pool.apply则是采用同步方式调用
+# 同步方式意味着下一个task需要等待上一个task完成后才能开始运行
+# 这显然不是我们想要的功能，所以采用异步方式连续地提交任务。
+
+
+pool.close()
+pool.join()
+# 调用join之前，先调用close函数，否则会出错。
+# 执行完close后不会有新的进程加入到pool
+# join函数等待所有子进程结束
+
+for i in results:
+    print(i.get())
+
+# if __name__ == "__main__":
